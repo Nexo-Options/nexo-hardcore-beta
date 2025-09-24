e54c52dea367902fc494d680a121ee82b353d385
Nexo Options <nexonexo712@gmail.com>
2025-09-24 22:08:50 +0300

    Create 05_stratagyStrip.ts

---
+++ deploy/00_nexoSingle/05_stratagyStrip.ts
@@ -0,0 +1,92 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types"
+import {utils} from "ethers"
+
+async function deployment(hre: HardhatRuntimeEnvironment): Promise<void> {
+  const {deployments, getNamedAccounts} = hre
+  const {deploy, get, execute} = deployments
+  const {deployer} = await getNamedAccounts()
+  const STRATEGY_ROLE = utils.keccak256(utils.toUtf8Bytes("STRATEGY_ROLE"))
+
+  const Pool = await get("NexoOperationalTreasury")
+  const PriceProviderETH = await get("PriceProviderETH")
+  const PriceProviderBTC = await get("PriceProviderBTC")
+
+  const paramsETH = {
+    pool: Pool.address,
+    priceProvider: PriceProviderETH.address,
+    spotDecimals: 18,
+    IVRate: 490000,
+    limit: 1000000e6,
+  }
+
+  const paramsBTC = {
+    pool: Pool.address,
+    priceProvider: PriceProviderBTC.address,
+    spotDecimals: 8,
+    IVRate: "59000000000000000",
+    limit: 1000000e6,
+  }
+
+  const WETHSTRIPPricer = await deploy("PriceCalculatorStripETH", {
+    contract: "PriceCalculator",
+    from: deployer,
+    log: true,
+    args: [paramsETH.IVRate, paramsETH.priceProvider],
+  })
+
+  const WBTCSTRIPPricer = await deploy("PriceCalculatorStripBTC", {
+    contract: "PriceCalculator",
+    from: deployer,
+    log: true,
+    args: [paramsBTC.IVRate, paramsBTC.priceProvider],
+  })
+
+  const strategyETH = await deploy("NexoStrategyStripETH", {
+    contract: "NexoStrategyStrip",
+    from: deployer,
+    log: true,
+    args: [
+      paramsETH.pool,
+      paramsETH.priceProvider,
+      WETHSTRIPPricer.address,
+      paramsETH.spotDecimals,
+      paramsETH.limit,
+    ],
+  })
+  await execute(
+    "NexoOperationalTreasury",
+    {log: true, from: deployer},
+    "grantRole",
+    STRATEGY_ROLE,
+    strategyETH.address,
+  )
+
+  const strategyBTC = await deploy("NexoStrategyStripBTC", {
+    contract: "NexoStrategyStrip",
+    from: deployer,
+    log: true,
+    args: [
+      paramsBTC.pool,
+      paramsBTC.priceProvider,
+      WBTCSTRIPPricer.address,
+      paramsBTC.spotDecimals,
+      paramsBTC.limit,
+    ],
+  })
+  await execute(
+    "NexoOperationalTreasury",
+    {log: true, from: deployer},
+    "grantRole",
+    STRATEGY_ROLE,
+    strategyBTC.address,
+  )
+}
+
+deployment.tags = ["single-test", "single-strip"]
+deployment.dependencies = [
+  "operational-treasury",
+  "options-manager",
+  "single-prices",
+]
+
+export default deployment
